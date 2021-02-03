0b07d967492bb499a6bdf09b9439b58383a79c12
Orlando <orlando.junior07@hotmail.com>
2021-02-03 00:36:21 -0300

    Realizando teste inserir conta com mesmo nome

--- src/main/java/br/ce/orlando/pages/ContasPage.java
+++ src/main/java/br/ce/orlando/pages/ContasPage.java
@@ -5,22 +5,27 @@ import org.openqa.selenium.By;
 import br.ce.orlando.core.BasePage;
 
 public class ContasPage extends BasePage {
-	
+
 	public void setNome(String nome) {
 		escrever("nome", nome);
 	}
-	
+
 	public void salvar() {
 		clicarBotao(By.xpath("//button[.='Salvar']"));
 	}
-	
-	public String obterMensagemSucesso(){
+
+	public String obterMensagemSucesso() {
 		return obterTexto(By.xpath("//div[@class='alert alert-success']"));
+	}
+
+	public String obterMensagemErro() {
+		return obterTexto(By.xpath("//div[@class='alert alert-danger']"));
 	}
 
 	public void clicarAlterarConta(String string) {
 		obterCelula("Conta", string, "Ações", "tabelaContas")
-			.findElement(By.xpath(".//span[@class='glyphicon glyphicon-edit']")).click();;		
+				.findElement(By.xpath(".//span[@class='glyphicon glyphicon-edit']")).click();
+		;
 	}
-	
+
 }
--- src/main/java/br/ce/orlando/tests/ContaTest.java
+++ src/main/java/br/ce/orlando/tests/ContaTest.java
@@ -32,4 +32,14 @@ public class ContaTest extends BaseTest {
 		
 		Assert.assertEquals("Conta alterada com sucesso!", contasPage.obterMensagemSucesso());
 	}
+	
+	@Test
+	public void testInserirContaMesmoNome() {
+		menuPage.acessarTelaInserirConta();
+		
+		contasPage.setNome("Conta do Teste alterada");
+		contasPage.salvar();
+		
+		Assert.assertEquals("Já existe uma conta com esse nome!", contasPage.obterMensagemErro());
+	}
 }
