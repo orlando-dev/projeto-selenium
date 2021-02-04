d177528632c4275e9ef51ad842e762b0d8d0e9c9
Orlando <orlando.junior07@hotmail.com>
2021-02-03 22:43:43 -0300

    Execucao dos testes como todo por prompt de comando e eclipse

--- src/main/java/br/ce/orlando/core/DriverFactory.java
+++ src/main/java/br/ce/orlando/core/DriverFactory.java
@@ -1,13 +1,21 @@
 package br.ce.orlando.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.ce.orlando.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 
 //	private static WebDriver driver;
+	
 	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
 		@Override
 		protected synchronized WebDriver initialValue() {
@@ -24,10 +32,25 @@ public class DriverFactory {
 	
 	public static WebDriver initDriver(){
 		WebDriver driver = null;
-		
-			switch (Propriedades.browser) {
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
 				case FIREFOX: driver = new FirefoxDriver(); break;
 				case CHROME: driver = new ChromeDriver(); break;
+			}
+		}
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+				case FIREFOX:cap = DesiredCapabilities.firefox(); break;
+				case CHROME: cap = DesiredCapabilities.chrome(); break;
+			}
+			
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.18.80:4444/wd/hub") , cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Falha na conexão com o GRID");
+				e.printStackTrace();
+			}
 		}
 		driver.manage().window().setSize(new Dimension(1200, 765));			
 		return driver;
--- src/main/java/br/ce/orlando/core/Propriedades.java
+++ src/main/java/br/ce/orlando/core/Propriedades.java
@@ -4,11 +4,18 @@ public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers browser = Browsers.CHROME;
+	public static Browsers BROWSER = Browsers.CHROME;
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 	
 	public enum Browsers {
 		CHROME,
 		FIREFOX
+	}
+	
+	public enum TipoExecucao{
+		LOCAL,
+		GRID
 	}
 
 }
